d98f7917640c7f775ac200b65411cf3e4ce37f95
nathanxli <nathan.nxli@gmail.com>
2025-04-02 08:20:38 -0400

    Created UserProfile model

--- studentconnect/network/models.py
+++ studentconnect/network/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=128)  # Placeholder for now
+
+    majors = models.CharField(max_length=200, help_text="Comma-separated if multiple")
+    academic_interests = models.TextField(help_text="Comma-separated list")
+    non_academic_interests = models.TextField(help_text="Comma-separated list")
+    current_classes = models.TextField(help_text="Comma-separated list")
+
+    def __str__(self):
+        return self.username
